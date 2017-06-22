b5467affe6ca1b56cf7a3bb15144c02e15466650
Mihir <patelm48@student.wpunj.edu>
2017-06-21 21:46:06 -0400

    Starter code

---
+++ TicTacToe.java
@@ -0,0 +1,17 @@
+
+public class TicTacToe {
+	
+	private static final int ROWS = 3;
+	private static final int COLS = 3;
+	private static final int NUM_OF_PLAYERS = 2;
+	private char [][] grid;
+	private char [] player_marks;
+	
+	TicTacToe() {
+		grid = new char[ROWS][COLS];
+		player_marks = new char[NUM_OF_PLAYERS];
+	}
+	
+	
+
+}
