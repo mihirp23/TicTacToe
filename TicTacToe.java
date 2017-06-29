c67d85ad194cdf3ece1e9fc557224845eb2db6fe
Mihir <patelm48@student.wpunj.edu>
2017-06-28 20:02:52 -0400

    Begin implementation

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,10 @@
+/* Name : Mihir Patel
+ * Date : June 28, 2017
+ * Desc : Implement a TicTacToe game between that is played with two players.
+ * File : TicTacToe.java
+ */
 
+import java.util.*;
 public class TicTacToe {
 	
 	private static final int ROWS = 3;
@@ -7,9 +13,41 @@ public class TicTacToe {
 	private char [][] grid;
 	private char [] player_marks;
 	
+	/* ---------------------- TicTacToe() -------------------------- */
 	TicTacToe() {
 		grid = new char[ROWS][COLS];
 		player_marks = new char[NUM_OF_PLAYERS];
+	}
+	
+	/* ---------------------- setBoard() --------------------------- */
+	public void setBoard() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				grid[i][j] = '-';
+			}
+		}
+	}
+	
+	/* ---------------------- displayBoard() ------------------------ */
+	public void displayBoard() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	/* ----------------------- setPlayerMarks() --------------------- */
+	public void setPlayerMarks() {
+		int i = 1;
+		while (i <= NUM_OF_PLAYERS) {
+			System.out.println("Player " + i + " enter your mark : ");
+			Scanner in = new Scanner(System.in);
+			String mark = in.next();
+			player_marks[i-1] = mark.charAt(0);
+			i++;
+		}
 	}
 	
 	
---
+++ TicTacToeGame.java
@@ -0,0 +1,16 @@
+/* Name : Mihir Patel
+ * Date : June 28, 2017
+ * Desc : Run the TicTacToe game between that is played with two players.
+ * File : TicTacToeGame.java
+ */
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+		game.setBoard();
+		game.displayBoard();
+		game.setPlayerMarks();
+
+	}
+
+}
