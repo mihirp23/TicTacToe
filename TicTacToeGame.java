1326d72ff8d41f4d54d532d503aebc6968522ed1
mihirp23 <mihirp38661@gmail.com>
2017-06-29 14:39:59 -0400

    Add more interaction with user

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,20 +6,42 @@
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
+		
+		// welcome message and instructions
+		System.out.println("Welcome to the game of Tic Tac Toe!");
+		System.out.println("Below, you will see a board with 9 tiles.");
+		System.out.println("When choosing, please choose an empty tile between 1 and 9.");
+		System.out.println();
+		System.out.println();
+		
+		// initial setup for games
 		TicTacToe game = new TicTacToe();
 		game.setBoard();
 		game.displayBoard();
 		game.setPlayerMarks();
+		
 		int player = 0;
+		// keep getting moves from users until game is over...
 		do {
+			System.out.println(player);
 			game.makeMove(player);
 			game.displayBoard();
+			// alternate player turns
 			player ^= 1;
-		    
-			
 		}while (!game.isGameOver());
 		
-
+		System.out.println();
+		System.out.println();
+		
+		// display winner
+		if (game.isGameWon()) {
+			player ^= 1;
+			System.out.println(game.getPlayerName(player) + " wins!");
+		}
+		else {
+			System.out.println("Game ended in a tie...");
+		}
+		
 	}
 
 }
